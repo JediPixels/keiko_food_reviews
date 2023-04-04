7fd267fbf6730f03248311c9711f060fa2b9fa44
JediPixels <43501910+JediPixels@users.noreply.github.com>
2023-04-04 17:52:01 -0400

    Modified the main function to ensure Widgets Flutter Binding first before initializing Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:keiko_food_reviews/firebase_options.dart';
 import 'package:keiko_food_reviews/helper/themes.dart';
 import 'package:keiko_food_reviews/pages/home.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
