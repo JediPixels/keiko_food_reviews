b94ef16fbbd6f8cd7db973a784514e32fdc56ad4
JediPixels <43501910+JediPixels@users.noreply.github.com>
2023-04-21 01:35:37 -0400

    Added the Storage Service class, the upload and delete photo methods.

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
+class StorageService {
+  static Future<String> uploadPhoto(
+      {required String uid, required XFile file}) async {
+    final Reference storageReference = FirebaseStorage.instance.ref();
+    final Reference photoReference =
+        storageReference.child('$uid/${file.name}');
+
+    try {
+      if (kIsWeb) {
+        Uint8List? fileAsBytes = await file.readAsBytes();
+        await photoReference.putData(fileAsBytes);
+      } else {
+        await photoReference.putFile(File(file.path));
+      }
+    } on Exception catch (error) {
+      debugPrint('Error Uploading Photo: $error');
+    }
+
+    String downloadURL = await photoReference.getDownloadURL();
+    return downloadURL;
+  }
+
+  static Future<void> deletePhoto({required String? photoPath}) async {
+    final Reference storageReferenceFromUrl =
+        FirebaseStorage.instance.refFromURL(photoPath!);
+
+    try {
+      await storageReferenceFromUrl
+          .delete()
+          .then((success) => debugPrint('Success'))
+          .onError((error, stackTrace) =>
+              debugPrint('Error Deleting Photo: $error'));
+    } on Exception catch (error) {
+      debugPrint('Error Deleting Photo: $error');
+    }
+  }
+}
