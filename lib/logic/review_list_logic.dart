e411f2be9cbd455aebac40c65f196a8d3eb97331
JediPixels <43501910+JediPixels@users.noreply.github.com>
2023-05-03 17:07:43 -0400

    Maintenance - Changing reviewModelList to a get.

--- lib/logic/review_list_logic.dart
+++ lib/logic/review_list_logic.dart
@@ -6,10 +6,11 @@ class ReviewListLogic {
   ReviewListLogic() : _uid = AuthenticationService.getCurrentUserUid();
 
   final String _uid;
-  late Stream<List<ReviewModel>> reviewModelList;
+  late Stream<List<ReviewModel>> _reviewModelListStream;
+  Stream<List<ReviewModel>> get reviewModelList => _reviewModelListStream;
 
   void getReviewModelList() {
-    reviewModelList = DatabaseService.getReviewList(_uid);
+    _reviewModelListStream = DatabaseService.getReviewList(_uid);
   }
 
   ReviewModel addReview() {
