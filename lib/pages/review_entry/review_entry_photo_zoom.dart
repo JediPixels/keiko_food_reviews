7018d482c972ad58326e61559f2ad1b8537a3a28
JediPixels <43501910+JediPixels@users.noreply.github.com>
2023-05-03 17:52:05 -0400

    Added the Review Entry Photo Zoom.

--- lib/pages/review_entry/review_entry_photo_zoom.dart
+++ lib/pages/review_entry/review_entry_photo_zoom.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:keiko_food_reviews/helper/arguments.dart';
+import 'package:keiko_food_reviews/models/review_model.dart';
 
 class ReviewEntryPhotoZoom extends StatelessWidget {
   const ReviewEntryPhotoZoom({super.key});
@@ -7,6 +10,65 @@ class ReviewEntryPhotoZoom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final ReviewEntryPhotoArguments reviewEntryPhotoArguments =
+        ModalRoute.of(context)?.settings.arguments as ReviewEntryPhotoArguments;
+    final ReviewModel reviewModel = reviewEntryPhotoArguments.reviewModel;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(reviewModel.title),
+        backgroundColor: Theme.of(context).colorScheme.surface.withOpacity(0.4),
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          InteractiveViewer(
+            child: Hero(
+              tag: '${reviewModel.reviewDate}',
+              // child: Image.network(
+              //   reviewModel.photo,
+              //   loadingBuilder: (
+              //     BuildContext context,
+              //     Widget image,
+              //     ImageChunkEvent? loadingProgress,
+              //   ) {
+              //     if (loadingProgress == null) return image;
+              //     return Center(
+              //       child: CircularProgressIndicator(
+              //         value: loadingProgress.expectedTotalBytes != null
+              //             ? loadingProgress.cumulativeBytesLoaded /
+              //                 loadingProgress.expectedTotalBytes!
+              //             : null,
+              //       ),
+              //     );
+              //   },
+              // ),
+              // Alternate way to load an image and cache it
+              child: CachedNetworkImage(
+                imageUrl: reviewModel.photo,
+                fit: BoxFit.fitWidth,
+                progressIndicatorBuilder: (
+                  BuildContext context,
+                  String url,
+                  DownloadProgress downloadProgress,
+                ) =>
+                    Center(
+                  child: CircularProgressIndicator(
+                    value: downloadProgress.progress,
+                  ),
+                ),
+                errorWidget: (
+                  BuildContext context,
+                  String url,
+                  dynamic error,
+                ) =>
+                    const Icon(Icons.error),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
